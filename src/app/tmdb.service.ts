8aeff12e153d274c62659343e38fd5c7aa0e9bdc
cayleatopolski <woahitscaylea@gmail.com>
2019-08-20 16:17:19 -0400

    minor styling

--- src/app/tmdb.service.ts
+++ src/app/tmdb.service.ts
@@ -6,37 +6,27 @@ import { Observable } from "rxjs";
   providedIn: "root"
 })
 export class TmdbService {
-
-
-
-  constructor(private http: HttpClient) { }
-   genres: any[];
-getMovieData(name: string, greaterThanOrLessThan: string, runTime: number, popularity: string): Observable<any>{
-    return this.http.get(`https://api.themoviedb.org/3/discover/movie?api_key=964781403df38499570b6f7233c0a524&language=en-US&with_genres=${name}&with_runtime.${greaterThanOrLessThan}=${runTime}&sort_by=vote_average.${popularity}
+  constructor(private http: HttpClient) {}
+  genres: any[];
+  getMovieData(
+    name: string,
+    greaterThanOrLessThan: string,
+    runTime: number,
+    popularity: string
+  ): Observable<any> {
+    return this.http
+      .get(`https://api.themoviedb.org/3/discover/movie?api_key=964781403df38499570b6f7233c0a524&language=en-US&with_genres=${name}&with_runtime.${greaterThanOrLessThan}=${runTime}&sort_by=vote_average.${popularity}
   `);
-
-
-
-}
-}
-
-
- 
-
-
-
+  }
   getTitleData(searchTerm: string): Observable<any> {
-
     return this.http
       .get(`https://api.themoviedb.org/3/search/movie?api_key=15e5712ff47e4688d1f70d94261a6c5d&query=${searchTerm}
   `);
   }
-
 
   getDetails(movieID: number): Observable<any> {
     return this.http
       .get(`https://api.themoviedb.org/3/movie/${movieID}?api_key=15e5712ff47e4688d1f70d94261a6c5d&language=en-US
       `);
   }
-
 }
