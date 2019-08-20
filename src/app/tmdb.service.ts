e4888f3c6f7439d25920a8d23b74128f09d4e42f
kevin balboa <kpbalboa@gmail.com>
2019-08-20 15:25:39 -0400

    genre search operational

--- src/app/tmdb.service.ts
+++ src/app/tmdb.service.ts
@@ -6,13 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TmdbService {
-genre: object[]= this.http.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=964781403df38499570b6f7233c0a524&language=en-US`);
   constructor(private http: HttpClient) { }
-
-getMovieData(genre: string, greaterThanOrLessThan: string, runTime: number, popularity: string): Observable<any>{
-  console.log((this.http.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=964781403df38499570b6f7233c0a524&language=en-US`)));
-  return this.http.get(`https://api.themoviedb.org/3/discover/movie?api_key=964781403df38499570b6f7233c0a524&language=en-US&with_genres=${genre}&with_runtime.${greaterThanOrLessThan}=${runTime}&sort_by=vote_average.${popularity}
+   genres: any[];
+getMovieData(name: string, greaterThanOrLessThan: string, runTime: number, popularity: string): Observable<any>{
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?api_key=964781403df38499570b6f7233c0a524&language=en-US&with_genres=${name}&with_runtime.${greaterThanOrLessThan}=${runTime}&sort_by=vote_average.${popularity}
   `);
+
+
+
 }
 }
-// /discover/movie?with_genres=878&with_cast=500&sort_by=vote_average.desc
