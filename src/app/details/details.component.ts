200003e6be518bd25f7e3bf3d08b22dbdd5c3192
Veronica <kisorv@gmail.com>
2019-08-20 15:41:34 -0400

    created Details component for additional details when you click on a movie

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import {TmdbService} from "../tmdb.service";
+
+@Component({
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.css"]
+})
+export class DetailsComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute, private tmdbService: TmdbService) {}
+movieData: object;
+
+showDetails(movieID: number): void {
+  this.tmdbService
+    .getDetails(movieID)
+    .subscribe(response => (this.movieData = response));
+}
+
+  ngOnInit() {
+    console.log(this.activatedRoute.snapshot.paramMap.get("id"));
+    this.showDetails(Number(this.activatedRoute.snapshot.paramMap.get("id")));
+  }
+}
--- src/app/tmdb.service.ts
+++ src/app/tmdb.service.ts
@@ -1,25 +1,48 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class TmdbService {
+  private readonly BASE_URL = "https://api.themoviedb.org/3";
+  private readonly API_KEY = "964781403df38499570b6f7233c0a524";
 
-constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-getMovieData(genre: string, greaterThanOrLessThan: string, runTime: number, popularity: string): Observable<any>{
-  console.log((this.http.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=964781403df38499570b6f7233c0a524&language=en-US`)));
-  return this.http.get(`https://api.themoviedb.org/3/discover/movie?api_key=964781403df38499570b6f7233c0a524&language=en-US&with_genres=${genre}&with_runtime.${greaterThanOrLessThan}=${runTime}&sort_by=vote_average.${popularity}
-  `);
-}
+  getMovieData(
+    genre: string,
+    greaterThanOrLessThan: string,
+    runTime: number,
+    popularity: string
+  ): Observable<any> {
+    console.log(
+      this.http.get(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=964781403df38499570b6f7233c0a524&language=en-US`
+      )
+    );
 
+    const url = `${this.BASE_URL}/discover/movie`;
+    const params = new HttpParams()
+      .set("api_key", this.API_KEY)
+      .set("language", "en-US")
+      .set("with_genres", genre)
+      .set(`with_runtime.${greaterThanOrLessThan}`, runTime.toString())
+      .set("sort_by", `vote_average.${popularity}`);
 
-getTitleData(searchTerm: string): Observable<any> {
-    console.log(searchTerm);
+    return this.http.get(url, { params });
+  }
+
+  getTitleData(searchTerm: string): Observable<any> {
     return this.http
       .get(`https://api.themoviedb.org/3/search/movie?api_key=15e5712ff47e4688d1f70d94261a6c5d&query=${searchTerm}
   `);
+  }
+
+  getDetails(movieID: number): Observable<any> {
+    return this.http
+      .get(`https://api.themoviedb.org/3/movie/${movieID}?api_key=15e5712ff47e4688d1f70d94261a6c5d&language=en-US
+      `);
   }
 }
